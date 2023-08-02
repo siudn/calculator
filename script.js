2d55ecfaef1d53760d4997c8a2bc097e89e33ef5
siudn <cshadx@gmail.com>
2023-08-02 16:10:08 -0400

    Created operate function and variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator;
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -5,3 +9,14 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const operate = (num1, num2, operator) => {
+    if (operator === "+")
+        add(num1, num2);
+    else if (operator === "-")
+        subtract(num1, num2);
+    else if (operator === "*")
+        multiply(num1, num2);
+    else if (operator === "/")
+        divide(num1, num2);
+}
