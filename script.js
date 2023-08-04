6b310e57b887a581ddc5669a3db9e90cc8b6b587
siudn <cshadx@gmail.com>
2023-08-04 00:26:27 -0400

    Buttons add number to display when clicked

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-let firstNumber = 0;
-let secondNumber = 0;
-let operator;
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const display = document.querySelector(".display");
+const equalButton = document.querySelector("#equal");
+const clearAll = document.querySelector("#AC");
+const clear = document.querySelector("#C");
+
+calcArray = [];
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calcArray.push(display.textContent);
+        calcArray.push(button.textContent);
+        display.textContent = '';
+    })
+})
+
+equalButton.addEventListener("click", () => {
+    calcArray.push(display.textContent);
+    operate(calcArray[0], calcArray[2], calcArray[1])
+})
+
+
+
 
 const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
+const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
 
 const operate = (num1, num2, operator) => {
@@ -20,3 +44,4 @@ const operate = (num1, num2, operator) => {
     else if (operator === "/")
         divide(num1, num2);
 }
+
